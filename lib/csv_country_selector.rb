923972745c8615e25a9d52eb3cb7d59206dcf050
Marco Metz <marco.metz@gmail.com>
2015-05-06 15:55:06 +0200

    EU Liste und methoden hinzugefügt

--- lib/csv_country_selector.rb
+++ lib/csv_country_selector.rb
@@ -10,6 +10,10 @@ module CsvCountrySelector
       #Lnd;Kfz;Sprache;ISO-Code;Kurzbez;Bezeichnung;Nationalität;Bezeichnung lang
       @@codes = ::CSV.read("#{File.dirname(File.expand_path(__FILE__))}/csv_country_selector/countries.csv", :col_sep => ";").inject({}){|a,b| a.merge!({b[0].force_encoding('UTF-8') => b[5].force_encoding('UTF-8')})}
 
+      #CSV Tabelle der Europäischen Länder
+      #Lnd;Kfz;Sprache;ISO-Code;Kurzbez;Bezeichnung;Nationalität;Bezeichnung lang
+      @@eu_codes = ::CSV.read("#{File.dirname(File.expand_path(__FILE__))}/csv_country_selector/eu_countries.csv", :col_sep => ";").inject({}){|a,b| a.merge!({b[0].force_encoding('UTF-8') => b[5].force_encoding('UTF-8')})}
+
       def self.short_name_for(name)
         @@codes.select{|k,v| v.downcase == name.downcase}.try(:first).try(:first)
       end
@@ -29,6 +33,10 @@ module CsvCountrySelector
 
       def self.codes
         @@codes
+      end
+
+      def is_in_eu?(country_short_name)
+        @@eu_codes.values.include?(country_short_name)
       end
 
     end
--- lib/csv_country_selector/version.rb
+++ lib/csv_country_selector/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module CsvCountrySelector
-  VERSION = "1.0.6"
+  VERSION = "1.0.7"
 end
